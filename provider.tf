7a8658fdc3e27b1252f0fa70d97868ef0a2a5669
srinivasdevops97 <birudurajusrinivas@gmail.com>
2025-10-06 16:45:59 +0530

    user profile

---
+++ provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+  backend "s3" {
+    bucket = "81s-remote-state123"
+    key    = "expense-vpcc"
+    region = "us-east-1"
+    dynamodb_table = "81s-locking"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
