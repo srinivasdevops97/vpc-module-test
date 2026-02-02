b53e281a8443efd032b1f5be91dc5aef87dda51e
srinivasdevops97 <birudurajusrinivas@gmail.com>
2026-02-02 19:54:41 +0530

    terraform

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,6 @@
 module "vpc" {
-    source = "../terraform-aws-vpc"
+    # source = "../terraform-aws-vpc"
+    source = "git::https://github.com/srinivasdevops97/terraform-aws-vpc.git?ref=main"
     project_name = var.project_name
     environment = var.environment
     common_tags = var.common_tags
